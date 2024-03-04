c74890980292705abce489989311f3f08126bcf9
sanitwasnik <84314505+sanitwasnik@users.noreply.github.com>
2024-03-04 16:25:37 +0530

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Image from 'next/image'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="p-4 border-b bg-black   text-emerald-50 grid justify-center gap-2 md:flex   " >
+          <div className="text-center pt-1"><h3 className=""> We`re hiring - Join our team!</h3></div>
+          <div className="text-center"><a href="https://www.omnisynkai.com/grow-with-us/careers"><button type="button" className="border-2 rounded-full border-indigo-600 p-1 hover:border-gray-400">⚡Open Positions</button></a></div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
+{/* <div className="p-4 border-b bg-black  text-emerald-50 flex justify-center gap-2  " >
+          <div className="text-center pt-1"><h3 className=""> We`re hiring - Join our team!</h3></div>
+          <div ><a href="https://www.omnisynkai.com/grow-with-us/careers"><button type="button" className="border-2 rounded-full border-indigo-600 p-1 hover:border-gray-400">⚡Open Positions</button></a></div>
+        </div> */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,194 @@
+"use client"
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className='w-full flex justify-center items-center flex-col  '>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+          <Image className=' object-contain p-2' src="/images/OmniSynkAILogo.png" alt='logo' width={200} height={200} />
+          <div className="hidden md:flex ">
+            <button
+              type='button'
+              onClick={() => window.open('https://github.com/sanitwasnik')}
+              className='black_btn m-1'>
+              Home
+
+            </button>
+            <button
+              type='button'
+              onClick={() => window.open('https://www.omnisynkai.com/mission')}
+              className='black_btn'>
+              Mission
+
+            </button>
+            <button
+              type='button'
+              onClick={() => window.open('https://www.omnisynkai.com/team')}
+              className='black_btn'>
+              Team
+
+            </button>
+            <button
+              type='button'
+              onClick={() => window.open('https://www.omnisynkai.com/grow-with-us')}
+              className='black_btn'>
+              Career
+
+            </button>
+            <button
+              type='button'
+              onClick={() => window.open('hhttps://www.omnisynkai.com/contact')}
+              className='black_btn'>
+              Contact
+
+            </button>
+          </div>
+
+        </nav>
+        <h1 className='head_text'>Managing your orders  <br className='max-md:hidden' />
+          has never been easier
+        </h1>
+        <h2 className='desc'>
+          With
+          <span className='orange_gradient font-bold'> OmniSynkAI</span>,
+          you can drive efficiency at scale while
+          actively receiving insights on growing your business.
+        </h2>
+        <div className=" items-center w-max">
+          <Image src="/images/peepsrunning.png" alt="" width={1000} height={1000} />
+        </div>
+      </header>
+      <div className=" w-full flex justify-center items-center flex-col px-8 py-14">
+        <h1 className='head_text'>Made for Everyone  <br className='max-md:hidden' />
+        </h1>
+        <h2 className='desc'>
+          With
+          <span className='orange_gradient font-bold'> OmniSynkAI</span>,
+          we offer tools for businesses at any size.   <br className='max-md:hidden' />
+          Business growth doesn`t stop at listing.</h2>
+
+        <button className="black_btn m-3">Request Demo</button>
+      </div>
+
+      <div className=" grid md:flex justify-between items-center w-full mb-10 px-8  py-8">
+        <div className="">
+          <h1 className='text-3xl font-medium'>Increase efficiency and revenue while cutting  <br className='max-md:hidden' />
+            costs on the fulfillment process.</h1>
+          <h2 className='desc'>
+            Our platform is a first of it’s kind tool
+            that allows sellers to use AI driven product
+            to help them navigate sales, insights,
+            cross-list and manage orders in one spot.</h2>
+        </div>
+        <div className="">
+          <Image src="/images/unboxing.png" alt="" width={400} height={400} />
+        </div>
+
+      </div>
+      <div className="w-full flex justify-center items-center flex-col px-8 py-8 divide-slate-400">
+
+        <Image src="/images/SectionCompanies.png" alt="" width={800} height={800} />
+      </div>
+
+
+      <div className="w-full flex justify-center items-center flex-col text-center">
+        <div>
+          <Image src="/images/HeyOmni.png" alt="" width={600} height={600} />
+
+        </div>
+        <div className="w-full flex justify-center items-center flex-col">
+          <h1 className='text-3xl font-medium'>Automate your business in a few steps  <br className='max-md:hidden' />
+          </h1>
+          <h2 className='desc'>Prioritize what truly counts and
+            leave the repetitive tasks to us.</h2>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className=" grid md:flex justify-between items-center w-full mb-10 px-8  py-8">
+        <div className="mt-20 flex flex-wrap">
+          <Image src="/images/Group.png" alt="" width={500} height={500} />
+        </div>
+        <div className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          <h1 className='text-3xl font-medium'>What we do best  <br className='max-md:hidden' />
+          </h1>
+          <h2 className="desc">Our platform streamlines order management for sellers of all sizes
+            by syncing tracking numbers, alerting to potential delays, and simplifying cross-listing.
+            Unlike other systems, we offer affordable, user-friendly solutions with 24/7 support,
+            making business growth less intimidating.
+
+          </h2>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="w-full flex justify-center items-center flex-col py-6">
+        <h1 className='head_text'>How it works  <br className='max-md:hidden' />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </h1>
+        <h2 className='desc'>
+          Our platform is meant to be simple and customized to fit your shop’s needs, no matter how big or small.
+        </h2>
+        <div className=" items-center">
+          <Image src="/images/how.png" alt="" width={1000} height={1000} />
+        </div>
       </div>
-    </main>
+
+      <div className=" grid md:flex justify-between items-center w-full mb-10 px-14  py-8 drop-shadow-2xl">
+        <div className="mt-20 flex flex-wrap">
+          <Image src="/images/peeps.png" alt="" width={500} height={500} />
+        </div>
+        <div className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          <h1 className='text-3xl font-medium'>Built by sellers, for sellers  <br className='max-md:hidden' />
+          </h1>
+          <h2 className="desc">Boost user satisfaction and streamline happiness. <br className='max-md:hidden' />
+            Designed for anyone - it’s simple.
+          </h2>
+        </div>
+      </div>
+
+
+      <div className=" grid md:flex justify-between items-center w-full mb-10 px-8  py-8">
+        <div className="">
+          <h1 className='text-3xl font-medium'>Who we are <br className='max-md:hidden' />
+          </h1>
+          <h2 className=' mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+            We`re a team of designers, researchers, and engineers -
+            with a shared vision to enhance efficiency, drive sales,
+            and make a meaningful impact on the lives of e-commerce
+            sellers nationwide.</h2>
+
+          <button className=" shadow-sm shadow-cyan-500/50 p-4  rounded-full hover:shadow-amber-500/50">Meet the team</button>
+        </div>
+        <div className="">
+          <Image src="/images/composition.png" alt="" width={500} height={500} />
+        </div>
+      </div>
+
+      <div className="bg-blue-600 w-full flex justify-center items-center flex-col px-10 py-14">
+        <h1 className='head_text2 pb-6'>Discover a better way to manage orders.  <br className='max-md:hidden' />
+        </h1>
+
+        <button className="black_btn m-3 pt-6">Visit us on indiegogo</button>
+      </div>
+      <div className="bg-blue-950 ">
+        <div className="flex justify-between px-12 py-14  items-center ">
+          <div>
+            <Image src="/images/OmniSynkAILogo.png" alt='logo' width={200} height={200} />
+          </div>
+          <div className="">
+            <ul className="list-none text-white ">
+              <li className="pb-1 hover:text-gray-600">Company</li>
+              <li className="pb-1 hover:text-gray-600">About</li>
+              <li className="pb-1 hover:text-gray-600">Our Team</li>
+              <li className="pb-1 hover:text-gray-600">Investors</li>
+              <li className="pb-1 hover:text-gray-600">Career-we`re hiring</li>
+              <li className="pb-1 hover:text-gray-600">Privacy Policy</li>
+            </ul>
+          </div>
+        </div>
+        <h2 className="text-white text-center py-10">Omni = Everywhere | Built with 💜 in Baltimore.</h2>
+
+      </div>
+    </>
   );
 }
